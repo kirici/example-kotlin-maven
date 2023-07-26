61f4e5e7cff7ccbacbd3b88ad83fa92fa1f84443
C. Burak Kirici <krcdotbrk@gmail.com>
2023-07-26 14:43:48 +0200

    Initial commit

---
+++ src/main/kotlin/hello.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    println("Hello, world!")
+
+    for (i in 1..100){
+        if ( i%15 == 0) {
+            println("FizzBuzz")}
+        else if(i%5 == 0) {
+            println("Buzz")}
+        else if(i%3 == 0){
+            println("Fizz")}
+        else {
+            println(i)
+        }
+
+    }
+}
